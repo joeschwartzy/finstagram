fc9561f9d3054dc783ffa06d6ae717469a82072a
Joe Schwartz <joe@vacuumsucks.com>
2017-05-31 03:47:09 +0000

    adding erb files

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,13 @@
 def humanized_time_ago(time_ago_in_minutes)
     if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes / 60} hours_ago"
+        "#{time_ago_in_minutes / 60} hours ago"
     else
-        "{time_ago_in_minutes} minutes_ago"
+        "#{time_ago_in_minutes} minutes ago"
     end
 end
 
 get '/' do
-    post_shark = {
+  @post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -15,11 +15,12 @@ get '/' do
     like_count: 0,
     comment_count: 1,
     comments: [{
-        username: "sharky_j",
-        text: "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
-  }]
-    }
-    post_whale = {
+      username: "sharky_j",
+      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    }]
+  }
+
+  @post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -32,7 +33,7 @@ get '/' do
     }]
   }
 
-  post_marlin = {
+  @post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -44,5 +45,11 @@ get '/' do
       text: "lunchtime! ;)"
     }]
   }
-  [post_shark, post_whale, post_marlin].to_s
+
+  [@post_shark, @post_whale, @post_marlin].to_s
+  @posts = [@post_shark, @post_whale, @post_marlin]
+
+  erb(:index)
 end
+
+
