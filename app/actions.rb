ecd2689caaaada57fafeecc905c51b04513dc8b0
Joe Schwartz <joe@vacuumsucks.com>
2017-06-21 00:24:19 +0000

    w5 d1

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,47 @@
+helpers do
+    def current_user
+        User.find_by(id:session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
+end
+
+post '/posts' do
+    params.to_s
 end
   
 get '/signup' do
     @user = User.new
     erb(:signup)
 end
-  
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+        session[:user_id] = user.id
+            redirect to('/')
+        else
+            @error_message ="Login failed."
+            erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
 post '/signup' do
   
     email = params[:email]
@@ -18,7 +52,7 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
    
     if @user.save
-      "User #{username} saved!"
+      redirect to('/login')
   else
       erb(:signup)
   end
